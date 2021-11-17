99dc80b421037773fd487124b75218804d3b8287
Silas Campbell <92600501+Silasssssss@users.noreply.github.com>
2021-11-17 08:18:02 -0600

    dark.js

---
+++ dark.js
@@ -0,0 +1,1237 @@
+var StyleCSS = `nav.joJglb {
+    background: rgb(40, 40, 40);
+}
+
+div.nhassd.asQXV.YVvGBb {
+    color: white;
+}
+
+div.ETRkCe {
+    background-color: rgb(40, 40, 40);
+}
+
+h2.oBSRLe {
+    color: white;
+}
+
+ol.JwPp0e {
+    background-color: rgb(40, 40, 40);
+}
+
+div.TQYOZc {
+    background-color: rgb(40, 40, 40);
+}
+
+div.SZ0kZe {
+    background-color: rgb(40, 40, 40);
+}
+
+div.bg6sud {
+    background-color: rgb(40, 40, 40);
+}
+
+a.IrxBzb.TpQm9d {
+    color: white;
+}
+
+div.EHzcec {
+    background: rgb(40, 40, 40)
+}
+
+a.NQV3m {
+    background: rgb(40, 40, 40);
+    color: black;
+}
+
+span.Rq5Gcb {
+    color: white
+}
+
+body {
+    background-color: rgb(40, 40, 40)
+}
+
+div.d4Fe0d {
+    background-color: rgb(30, 30, 30);
+    color: white
+}
+
+h2.EZrbnd {
+    color: white
+}
+
+div.lziZub,
+.lziZub:visited {
+    color: white
+}
+
+a.onkcGd.uBUej.VnOHwf-Tvm9db.zZN2Lb-Wvd9Cc.VBEdtc-Wvd9Cc.EZrbnd {
+    color: white
+}
+
+div.ZjCSDe.dDKhVc.pco8Kc {
+    color: white
+}
+
+div.Vlohie.asQXV.YVvGBb {
+    color: white
+}
+
+div.jrhqBd.LBlAUc.Aopndd.ZoT1D.TIunU {
+    background-color: rgb(30, 30, 30);
+    color: white
+}
+
+div.pOf0gc.QRiHXd.Aopndd.cKrCw {
+    background-color: rgb(30, 30, 30);
+    color: white
+}
+
+div.A6dC2c.bKJwEd.VBEdtc-Wvd9Cc.zZN2Lb-Wvd9Cc {
+    color: white
+}
+
+div.tmMkWb.ppMo6b {
+    color: white
+}
+
+div.bswVrf.Lzdwhd-BrZSOd {
+    color: white
+}
+
+div.LsqTRb.Lzdwhd-AyKMt.tgNIJf-Wvd9Cc.Yiql6e.iTy5c.editable {
+    color: white
+}
+
+div.QRiHXd.IMvYId.fXuRkd.UISY8d-Ysl7Fe.VnOHwf-Wvd9Cc {
+    color: white
+}
+
+div.onkcGd.YVvGBb.IMvYId.P3W0Dd-Ysl7Fe.zFfAHb {
+    color: white
+}
+
+a.onkcGd,
+a.onkcGd:visited {
+    color: white
+}
+
+span.sCv5Q.asQXV {
+    color: white
+}
+
+h2.VnOHwf-Tvm9db.XjYjO.nforOe.dRIMEd {
+    color: white
+}
+
+td.iFGZdc.gQZxn.njieAf.asQXV {
+    color: white
+}
+
+h2.VnOHwf-Tvm9db.XjYjO {
+    color: white
+}
+
+div.VnOHwf-Tvm9db.asQXV.jg22B {
+    color: white
+}
+
+a.onkcGd.zZN2Lb-Wvd9Cc.VBEdtc-Wvd9Cc.apFsO {
+    color: white
+}
+
+.Aopndd {
+    background-color: rgb(40, 40, 40)
+}
+
+.n8F6Jd {
+    color: white
+}
+
+div.qhnNic.LBlAUc.Aopndd.TIunU {
+    background-color: rgb(30, 30, 30)
+}
+
+span.YVvGBb.asQXV {
+    color: white
+}
+
+span.IMvYId.dDKhVc.YVvGBb {
+    color: white
+}
+
+span.l4V7wb.Fxmcue {
+    color: white
+}
+
+div.VSWCL.tLDEHd {
+    color: white
+}
+
+span.dDKhVc {
+    color: white
+}
+
+a.gJItbc.asQXV {
+    color: white
+}
+
+div.FLgDTb.pEwOBc.Aopndd {
+    background-color: rgb(40, 40, 40)
+}
+
+h2.ipmZkd.XjYjO {
+    color: white
+}
+
+h3.asQXV {
+    color: white
+}
+
+h3.asQXV {
+    color: white
+}
+
+div.tLDEHd {
+    color: white
+}
+
+h3.asQXV.NXcbkc {
+    color: white
+}
+
+h3.X65jac.rpo4wf {
+    color: white
+}
+
+h3.X65jac.rpo4wf {
+    color: white
+}
+
+div.ViCi4 {
+    color: white
+}
+
+p.tLDEHd {
+    color: white
+}
+
+span.PtHAPb.asQXV {
+    color: white
+}
+
+h2.EfLcNb.XjYjO {
+    color: white
+}
+
+h3.rpo4wf {
+    color: white
+}
+
+div.gKkZCe {
+    background-color: rgb(40, 40, 40)
+}
+
+div.KeAbHb {
+    background-color: rgb(40, 40, 40)
+}
+
+span.UYUfn.oJeWuf.wnIM7.fb0g6.d3Fkdd {
+    background-color: rgb(40, 40, 40)
+}
+
+div.ECOutf.Aopndd.najF6d {
+    background-color: rgb(30, 30, 30)
+}
+
+p.WdYux {
+    color: white
+}
+
+div.YVvGBb.asQXV {
+    color: white
+}
+
+div.YVvGBb.dDKhVc {
+    color: white
+}
+
+div.WOPwXe {
+    color: white
+}
+
+div.A6dC2c {
+    color: white
+}
+
+ul.tLDEHd {
+    color: white
+}
+
+div.zaxms.NyxV9b.Aopndd {
+    background-color: rgb(30, 30, 30)
+}
+
+div.NFUcsb.df5yGe.qRUolc {
+    color: white
+}
+
+span.UYUfn.oJeWuf.wnIM7.fb0g6.DyGAsf {
+    color: white
+}
+
+div.I7OXgf.ZEeHrd.Inn9w.iWO5td {
+    background-color: rgb(40, 40, 40)
+}
+
+p {
+    color: white
+}
+
+label.fyExH {
+    background-color: rgb(40, 40, 40)
+}
+
+div.W8M4Ad {
+    color: white
+}
+
+div.bqKF7d.pco8Kc {
+    color: white
+}
+
+div.K6Ovqd {
+    color: white
+}
+
+div.qk0lee.QRiHXd.VBEdtc-Wvd9Cc {
+    background-color: rgb(30, 30, 30)
+}
+
+div.hgjBDc.e8E4Gb {
+    background-color: rgb(30, 30, 30)
+}
+
+textarea.hqfVKd.tL9Q4c {
+    background-color: rgb(30, 30, 30);
+    color: white
+}
+
+div.CYSZae.snByac {
+    color: white
+}
+
+div.Yalane.oJeWuf {
+    background-color: rgb(30, 30, 30)
+}
+
+div.fWf7qe:not(.RDPZE):hover .Yalane {
+    background-color: rgb(30, 30, 30)
+}
+
+div.po4Aib.QRiHXd.Aopndd {
+    background-color: rgb(30, 30, 30)
+}
+
+div.qj5L0 {
+    color: white
+}
+
+div.Evt7cb {
+    color: white
+}
+
+div.asQXV {
+    color: white
+}
+
+span.UYUfn.oJeWuf.wnIM7.fb0g6 {
+    color: white
+}
+
+div.UISY8d-Ysl7Fe:hover {
+    background-color: rgb(30, 30, 30)
+}
+
+div.KmLLod:hover.uO32ac {
+    background-color: rgb(30, 30, 30)
+}
+
+div.r0VQac.QRiHXd.Aopndd {
+    background-color: rgb(30, 30, 30)
+}
+
+div.pOf0gc.QRiHXd.Aopndd.M4LFnf {
+    background-color: rgb(30, 30, 30)
+}
+
+div.OK1tJe.R5iLrf.bJuVn {
+    background-color: rgb(30, 30, 30);
+    color: white
+}
+
+div.UjXaMc.FMzRdf {
+    color: white
+}
+
+div.jlfrG.C4yF5e.R5iLrf.bJuVn {
+    color: white
+}
+
+div.jWCzBe.QRiHXd.idtp4e.u73Apc {
+    background-color: transparent
+}
+
+div.jlfrG:before {
+    background: linear-gradient(to bottom, rgb(30, 30, 30), transparent)
+}
+
+div.jlfrG:after {
+    background: linear-gradient(to top, rgb(30, 30, 30), transparent)
+}
+
+*::-webkit-scrollbar {
+    width: 10px;
+}
+
+*::-webkit-scrollbar-track {
+    background: rgb(30, 30, 30);
+}
+
+*::-webkit-scrollbar-thumb {
+    background-color: white;
+    border-radius: 20px;
+    border: 3px solid gray;
+}
+
+span.DShyMc-MjM2ODMxNjcxNjIy {
+    background-color: transparent
+}
+
+li.tUJKGd:not(.xp2dJ).ndcsBf {
+    background-color: transparent;
+    color: black
+}
+
+div.idtp4e {
+    background-color: transparent !important
+}
+
+li.tUJKGd:not(.xp2dJ):not(.rZXyy):hover {
+    background-color: rgb(30, 30, 30) !important
+}
+
+body.DShyMc-MjM2ODMxNjcxNjIy li.tUJKGd:not(.xp2dJ).ndcsBf div.idtp4e {
+    background-color: transparent
+}
+
+div.nforOe.mSQBve.Aopndd {
+    background-color: rgb(30, 30, 30)
+}
+
+p.VnOHwf-Tvm9db.rpo4wf {
+    color: white
+}
+
+span.asQXV.CeZYxc {
+    color: white
+}
+
+p.D9l13b.dDKhVc {
+    color: white
+}
+
+p.asQXV {
+    color: white
+}
+
+div.YVvGBb {
+    color: white
+}
+
+span.asQXV.QRiHXd {
+    color: white
+}
+
+span.rpo4wf {
+    color: white
+}
+
+div.JPdR6b.e5Emjc.hVNH5c.qjTEB {
+    background-color: rgb(30, 30, 30)
+}
+
+svg.iqM9Yd.xSP5ic.NMm5M {
+    color: white;
+    fill: white
+}
+
+span.z80M1.FeRvI {
+    color: white
+}
+
+h3.quka1c.dDKhVc-Wvd9Cc {
+    color: white
+}
+
+div.TNg8Ce {
+    background: rgb(40, 40, 40)
+}
+
+span.kx3Hed.VZhFab {
+    color: white
+}
+
+span.DPvwYc.iqM9Yd.xSP5ic {
+    color: white;
+    fill: white
+}
+
+div.XfQwid.lziZub.xd70xe.n0p5v.V8apv {
+    background-color: rgb(30, 30, 30)
+}
+
+div.tLDEHd.YVvGBb {
+    color: white
+}
+
+div.IzVHde.n0p5v {
+    background-color: rgb(30, 30, 30)
+}
+
+span.YVvGBb.A6dC2c {
+    color: white
+}
+
+a.u2mfde.hN1OOc.EZrbnd.J1raN.S6Vdac {
+    color: white
+}
+
+span.xSP5ic {
+    color: white;
+    fill: white;
+}
+
+div.yawtRb.MO2Wde {
+    background: rgb(30, 30, 30)
+}
+
+div.JPdR6b.qjTEB {
+    background-color: rgb(30, 30, 30)
+}
+
+span.z80M1.FwR7Pc {
+    background-color: transparent
+}
+
+div.JBMs6 {
+    background-color: rgb(30, 30, 30)
+}
+
+span.IqJTee {
+    color: white;
+}
+
+div.uArJ5e.Y5FYJe.cjq2Db.sEZiv.TYHMlb.oxacD.M9Bg4d {
+    color: white;
+    fill: white;
+}
+
+svg.gb_Xe {
+    color: white;
+}
+
+div.uArJ5e.Y5FYJe.cjq2Db.xSP5ic.oxacD.M9Bg4d {
+    color: white;
+    fill: white;
+}
+
+div.JDxyrc.xSP5ic {
+    color: white;
+    fill: white;
+}
+
+div.uArJ5e.Y5FYJe.cjq2Db.ORmQmd.ARrCac.pPQgvf.M9Bg4d {
+    color: white;
+    fill: white;
+}
+
+div.SNYuNc.ciEgyd {
+    color: white
+}
+
+div.uArJ5e.Y5FYJe.cjq2Db.MRFPwb.oxacD.rKc6P.M9Bg4d {
+    color: white;
+    fill: white;
+}
+
+div.U26fgb.JRtysb.WzwrXb.I12f0b.K2mXPb {
+    color: white;
+    fill: white;
+}
+
+style.DShyMc-Mjk4MjAxNzE5Nzgw .tUJKGd:not(.xp2dJ).ndcsBf.boxOzd,
+.DShyMc-Mjk4MjAxNzE5Nzgw .tUJKGd:not(.xp2dJ).ndcsBf.idtp4e,
+.DShyMc-Mjk4MjAxNzE5Nzgw .tUJKGd:not(.xp2dJ).ndcsBf .boxOzd,
+.DShyMc-Mjk4MjAxNzE5Nzgw .tUJKGd:not(.xp2dJ).ndcsBf .idtp4e,
+.DShyMc-Mjk4MjAxNzE5Nzgw .ZoT1D.ndcsBf.boxOzd,
+.DShyMc-Mjk4MjAxNzE5Nzgw .ZoT1D.ndcsBf.idtp4e,
+.DShyMc-Mjk4MjAxNzE5Nzgw .ZoT1D.ndcsBf .boxOzd,
+.DShyMc-Mjk4MjAxNzE5Nzgw .ZoT1D.ndcsBf .idtp4e {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+span.u7S8tc.YVvGBb {
+    color: white;
+}
+
+div.jgvuAb {
+    color: white !important;
+    background: rgb(30, 30, 30) !important;
+}
+
+div.ncFHed {
+    background: rgb(30, 30, 30) !important;
+}
+
+span.Xp0OCe .LMgvRb .oJeWuf,
+.Xp0OCe .OA0qNb .oJeWuf {
+    color: white !important;
+}
+
+div.ncFHed .MocG8c.KKjvXb {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+h2.WOPwXe {
+    color: white !important;
+}
+
+div.bFjUmb-Ysl7Fe {
+    background-color: rgba(26, 115, 232, 0.122) !important;
+}
+
+span.vRMGwf,
+span.oJeWuf {
+    color: white !important;
+}
+
+a.fKz7Od,
+a.TpQm9d {
+    color: white !important;
+}
+
+div.Aopndd {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.wQuPk .wMvEnb,
+.wQuPk .wMvEnb.pCcXPe,
+.wQuPk .wMvEnb.N4XV7d {
+    color: white !important;
+}
+
+div.wQuPk .JsqLM.pCcXPe {
+    color: white !important;
+}
+
+div.wQuPk .JsqLM.N4XV7d {
+    color: white !important;
+}
+
+div.i5sehe {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.dDKhVc {
+    color: white !important;
+}
+
+div.NZ9wdc {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+img.Tz7kj {
+    content: url(https://svgshare.com/i/WqY.svg) !important;
+}
+
+div.pO05gd {
+    color: white !important;
+}
+
+h2.tLDEHd {
+    color: white !important;
+}
+
+img.OjU4Mb {
+    content: url(https://svgshare.com/i/WrC.svg) !important;
+}
+
+div.qTs5Xc {
+    color: white !important;
+    background-color: rgb(30, 30, 30) !important;
+}
+
+input.poFWNe {
+    background-color: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+div.vPNssb .jBmls {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.jaXlcf {
+    color: white !important;
+}
+
+div.cjq2Db {
+    color: white !important;
+    fill: white !important;
+}
+
+div.zTrXGf {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.fidHdf {
+    border-left: 2px solid white !important;
+}
+
+a.u2mfde.hN1OOc.EZrbnd.GblU6b.eumXzf.VnOHwf-Tvm9db.P3W0Dd-Ysl7Fe.UISY8d-Ysl7Fe.S6Vdac:hover {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.wZTANe:hover {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.feojCc {
+    background-color: rgb(40, 40, 40) !important;
+    color: white !important;
+}
+
+div.kCtYwe {
+    border-top: 1px solid white !important;
+}
+
+svg.xSP5ic {
+    color: white !important;
+    fill: white !important;
+}
+
+div.vnnr5e .I9OJHe {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.vnnr5e .CIy9F {
+    background-color: rgb(30, 30, 30) !important;
+    border-top: 2px solid white !important;
+}
+
+div.vnnr5e .snByac {
+    color: white !important;
+}
+
+div.vnnr5e:not(div.RDPZE):hover .I9OJHe,
+div.vnnr5e:not(div.RDPZE):hover .CIy9F {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.vnnr5e .Aworge,
+div.XpxsVb .Aworge {
+    color: white !important;
+}
+
+div.vnnr5e .Aworge:hover,
+div.XpxsVb .Aworge:hover {
+    color: rgb(35, 35, 35) !important;
+}
+
+p.tLDEHd-Wvd9Cc {
+    color: white !important;
+}
+
+div.p0oLxb.RDPZE {
+    color: white !important;
+}
+
+div.cLpBac {
+    background: rgb(30, 30, 30) !important;
+}
+
+span.nw8Rve .DPvwYc {
+    color: white !important;
+}
+
+div.eZFVZ .je6t8b:not(div.RDPZE),
+div.eZFVZ .je6t8b:not(div.RDPZE) .oJeWuf,
+div.eZFVZ .je6t8b:not(div.RDPZE).u3bW4e .oJeWuf,
+div.eZFVZ .je6t8b:not(div.RDPZE):hover .oJeWuf {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.asQXV-FGzYL {
+    color: white !important;
+}
+
+div.VOEIyf,
+div.VOEIyf .jBmls,
+div.oKubKe {
+    color: white !important;
+}
+
+span.tLDEHd {
+    color: white !important;
+}
+
+svg.p0oLxb {
+    color: white !important;
+}
+
+span.nw8Rve .DPvwYc {
+    color: white !important;
+}
+
+div.ybOdnf .eU809d {
+    border-color: white transparent;
+}
+
+div.DShyMc-MzI5NDg0OTczODYz .ybOdnf:not(div.RDPZE).iWO5td {
+    background-color: transparent !important;
+}
+
+div.y2d25 {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.ECK80e {
+    color: white !important;
+}
+
+div.dKKcxf,
+div.STMvPe {
+    color: white !important;
+    background: rgb(30, 30, 30) !important;
+}
+
+div.NE9bBb {
+    background: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+input.O98Lj {
+    color: white !important;
+}
+
+div.XaepId {
+    background: rgb(35, 35, 35) !important;
+    color: white !important;
+}
+
+svg.NMm5M {
+    fill: white !important;
+}
+
+th.XaepId-mrubje {
+    color: white !important;
+}
+
+td.XaepId-gElRsf {
+    color: white !important;
+}
+
+p.A6dC2c {
+    color: white !important;
+}
+
+div.KxwMuc {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.BUzZed {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.H9eFkc {
+    color: white !important;
+}
+
+div.mRRbif {
+    color: white !important;
+}
+
+div.vB8bIb {
+    color: white !important;
+}
+
+div.goTwYb {
+    color: white !important;
+}
+
+div.IMvYId,
+div.IMvYId:visited {
+    color: white !important;
+}
+
+div.WHShXb {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.hPriQc {
+    color: white !important;
+}
+
+div.ZjEvzc {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+input.Ebw6d {
+    color: white !important;
+}
+
+div.IMV4Cb {
+    color: white !important;
+}
+
+span.SjYFRc {
+    color: white !important;
+}
+
+div.QT8Hbf {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.QT8Hbf:hover {
+    background: rgb(35, 35, 35) !important;
+}
+
+div.JE7b9e {
+    color: white !important;
+}
+
+div.b0ZcJb {
+    color: white !important;
+}
+
+div.ftP2Gf {
+    background-color: rgb(35, 35, 35) !important;
+}
+
+svg.oFt7s {
+    fill: white !important;
+}
+
+td.TAjiIf {
+    background: rgb(30, 30, 30) !important;
+}
+
+td.pNoxUe {
+    background: rgb(30, 30, 30) !important;
+}
+
+a.Evt7cb,
+a.Evt7cb:visited {
+    color: white !important;
+}
+
+tr.TJtJXb:hover,
+tr.tYQn5c:hover,
+tr.DC55n:hover {
+    background: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+tr.DC55n td {
+    color: white !important;
+    background: rgb(30, 30, 30) !important;
+}
+
+span.ppMo6b {
+    color: white !important;
+}
+
+span.asQXV {
+    color: white !important;
+}
+
+span.cL8LOd {
+    border-bottom: .0625rem solid white !important;
+}
+
+div.Ax4B8,
+div.yNVtPc {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+input.whsOnd {
+    color: white !important;
+}
+
+div.qEVLZc {
+    color: white !important;
+}
+
+div.nk37z {
+    color: white !important;
+}
+
+div.hYt5f.EfvGO .WkZsyc {
+    color: white !important;
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.ybOdnf:not(div.RDPZE).iWO5td {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.RpYYWb {
+    background-color: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+div.Kqfayb {
+    color: white !important;
+}
+
+span.RMrItf {
+    color: white !important;
+}
+
+div.VOEIyf .jBmls {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+div.oKubKe[aria-selected="true"] {
+    background-color: transparent !important;
+}
+
+div.tl5cWb {
+    color: white !important;
+}
+
+div.ybOdnf:not(div.RDPZE).iWO5td {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+tr.d6CWTd.lkqKH.UISY8d-Ysl7Fe.P3W0Dd-Ysl7Fe.qRU9Ec:hover {
+    background: rgb(30, 30, 30) !important;
+}
+
+p.ViCi4 {
+    color: white !important;
+}
+
+p.dDKhVc {
+    color: white !important;
+}
+
+p.ppMo6b {
+    color: white !important;
+}
+
+span.uRr8Jb:not(.RDPZE) .snByac {
+    color: white !important;
+}
+
+div.uQ3ESd {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+span.IMvYId,
+span.IMvYId:visited {
+    color: white !important;
+}
+
+span.CHXfbe {
+    color: white !important;
+}
+
+span.nk37z {
+    color: white !important;
+}
+
+span.udxSmc {
+    color: white !important;
+}
+
+span.Kqfayb {
+    color: white !important;
+}
+
+div.x4sAde,
+div.x4sAde:visited {
+    color: white !important;
+}
+
+span.Evt7cb,
+span.Evt7cb:visited {
+    color: white !important;
+}
+
+div.JgVABb {
+    color: white !important;
+}
+
+div.zuzKle .KRoqRc {
+    color: white !important;
+}
+
+span.lziZub,
+span.lziZub:visited {
+    color: white !important;
+}
+
+div.gb_k div.gb_lb {
+    color: white !important;
+}
+
+div.gb_k div.gb_nb {
+    color: white !important;
+}
+
+a.gb_rb.gb_rb {
+    background-color: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+a.gb_rb.gb_rb:hover,
+.gb_rb.gb_rb:active {
+    background-color: rgb(35, 35, 35) !important;
+}
+
+div.gb_E {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.gb_0b,
+div.gb_2b {
+    color: white !important;
+}
+
+a.gb_Lb:hover {
+    background: rgb(30, 30, 30) !important;
+}
+
+a.gb_vb.gb_vb {
+    background-color: rgb(30, 30, 30) !important;
+    color: white !important;
+}
+
+a.gb_vb.gb_vb:hover {
+    background-color: rgb(35, 35, 35) !important;
+}
+
+svg.gb_xb path {
+    color: white !important;
+}
+
+a.gb_tb.gb_tb {
+    color: white !important;
+    background: rgb(30, 30, 30) !important;
+}
+
+a.gb_tb.gb_tb:hover {
+    color: white !important;
+    background: rgb(35, 35, 35) !important;
+}
+
+a.gb_Cb.gb_Cb {
+    color: white !important;
+}
+
+a.gb_Cb.gb_Cb,
+a.gb_Db.gb_Db {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+a.gb_Cb.gb_Cb:hover,
+a.gb_Db.gb_Db:hover {
+    background-color: rgb(35, 35, 35) !important;
+}
+
+svg.gb_Ve path {
+    color: white !important;
+}
+
+div.gb_Wd.gb_Sa.gb_Id div iframe #document html body.EIlDfe div.MCcOAc.IqBfM.LcUz9d.b30Rkd.e2G3Fb.d8Etdd.cjGgHb c-wiz.SSPGKf div.T4LgNb.eejsDc div.kFwPee c-wiz div.qWuU9c div.EHzcec.eejsDc {
+    background: rgb(30, 30, 30) !important;
+}
+
+li.QRiHXd.IMvYId.fXuRkd.UISY8d-Ysl7Fe.VnOHwf-Wvd9Cc:hover,
+li.QRiHXd.IMvYId.fXuRkd.UISY8d-Ysl7Fe.VnOHwf-Wvd9Cc:active {
+    background-color: rgb(30, 30, 30) !important;
+}
+
+li.QRiHXd.IMvYId.fXuRkd.UISY8d-Ysl7Fe:hover,
+li.QRiHXd.IMvYId.fXuRkd.UISY8d-Ysl7Fe:active {
+    background: rgb(30, 30, 30);
+}
+
+div.xPAMbf.boxOzd.UISY8d-Ysl7Fe.UvQypf:hover {
+    background: rgb(30, 30, 30) !important;
+}
+
+div.JsqLM.pCcXPe {
+    color: white !important;
+}
+
+div.JsqLM.N4XV7d {
+    color: white !important;
+}
+
+div.gb_4b {
+    color: white !important;
+}
+
+a.gb_C[aria-expanded=true] .gb_Ve,
+a.gb_C[aria-expanded=true] .gb_We {
+    fill: white !important;
+}
+
+html.hcfe {
+    background-color: rgb(40, 40, 40) !important;
+}
+
+@media (min-width: 48.1875rem) {
+    div.hcfe {
+        background: rgb(40, 40, 40) !important;
+    }
+}
+
+html.hcfe>header {
+    background-color: rgb(40, 40, 40) !important;
+}
+
+div.gaiabar {
+    background: rgb(40, 40, 40) !important;
+}
+
+div.gb_uc svg {
+    color: white !important;
+}
+
+div#material-bar-custom-product-name a {
+    color: white !important;
+}
+
+a.YVvGBb.xUYklb.VnOHwf-Tvm9db.XjYjO {
+    color: white !important;
+}
+
+span.RdyDwe.snByac {
+    color: white !important;
+}
+
+h1.fOvfyc.XjYjO.VnOHwf-Tvm9db {
+    color: white !important;
+}
+
+span {
+  color: white !important;
+}
+`;
+
+var stylesheet = document.createElement("style");
+stylesheet.innerHTML = StyleCSS;
+document.head.appendChild(stylesheet);
